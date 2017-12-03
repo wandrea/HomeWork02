54d0208031a794d0c284b8f3f524969b0f2d5052
wandrea <wagenhofferandrea@gmail.com>
2017-12-03 11:07:02 +0100

    #2

--- src/hu/helixlab/CalculatorService.java
+++ src/hu/helixlab/CalculatorService.java
@@ -6,7 +6,7 @@ package hu.helixlab;
 public class CalculatorService {
 
 
-    /* isItEven metódus készítése, mely boolean típusú és publikus,
+    /*Publikus isItEven metódus készítése, melynek visszatérési értéke boolean típusú,
      egy int típusú változó a bemenõ paramétere, maradékos osztással eldönthetõ, hogy páros vagy páratlan a szám, ha páros akkor visszatér true-val,
      ha páratlan akkor false lesz a visszatérési érték.*/
 
@@ -14,5 +14,22 @@ public class CalculatorService {
         return number % 2 == 0;
     }
 
-
+    /*A calculate metódus két int típusú és egy char típusú bemenõ paramétert vár,
+    a visszatérési értéke Float típusú, arra az esetre, ha nem ismerhetõ fel az operation paraméterben lévõ karakter.
+    A switch szerkezetben megvizsgáljuk az operation paraméterben található karaktert és ha az alapmûveletek közül valamelyik, akkor elvégezzük a mûveletet.
+    A mûveletek eredménye a visszatérési típusra lettek kasztolva.*/
+    public Float calculate(int x, int y, char operation){
+        switch (operation){
+            case '+':
+                return (float)x+y;
+            case '-':
+                return (float)x-y;
+            case '*':
+                return (float)x*y;
+            case '/':
+                return (float)x/y;
+            default:
+                return null;
+        }
+    }
 }
