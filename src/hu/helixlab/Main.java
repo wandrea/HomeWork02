0d8d73979bc07e4aefb9ab18507c923991602613
wandrea <wagenhofferandrea@gmail.com>
2017-12-03 12:39:21 +0100

    Létrehozott metódusok meghívása.

--- src/hu/helixlab/Main.java
+++ src/hu/helixlab/Main.java
@@ -3,6 +3,16 @@ package hu.helixlab;
 public class Main {
 
     public static void main(String[] args) {
+        CalculatorService cs = new CalculatorService();
 
+        System.out.println(cs.isItEven(25));
+        System.out.println(cs.calculate(5, 6, '+'));
+
+        cs.generateRandomNumber();
+
+        cs.generateAndCompareRandomNumbers();
+        System.out.println();
+        CalculatorHelper ch = new CalculatorHelper();
+        ch.doCalculate();
     }
 }
