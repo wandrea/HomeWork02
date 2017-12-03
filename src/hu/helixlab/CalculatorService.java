97830fc17f8c39b86af203063415604cf238b230
wandrea <wagenhofferandrea@gmail.com>
2017-12-03 12:17:30 +0100

    #4

--- src/hu/helixlab/CalculatorService.java
+++ src/hu/helixlab/CalculatorService.java
@@ -3,6 +3,8 @@ package hu.helixlab;
 /* CalculatorService nevü osztály készítése, mely publikus,*/
 
 
+import java.util.Random;
+
 public class CalculatorService {
 
 
@@ -18,18 +20,32 @@ public class CalculatorService {
     a visszatérési értéke Float típusú, arra az esetre, ha nem ismerhetõ fel az operation paraméterben lévõ karakter.
     A switch szerkezetben megvizsgáljuk az operation paraméterben található karaktert és ha az alapmûveletek közül valamelyik, akkor elvégezzük a mûveletet.
     A mûveletek eredménye a visszatérési típusra lettek kasztolva.*/
-    public Float calculate(int x, int y, char operation){
-        switch (operation){
+    public Float calculate(int x, int y, char operation) {
+        switch (operation) {
             case '+':
-                return (float)x+y;
+                return (float) x + y;
             case '-':
-                return (float)x-y;
+                return (float) x - y;
             case '*':
-                return (float)x*y;
+                return (float) x * y;
             case '/':
-                return (float)x/y;
+                return (float) x / y;
             default:
                 return null;
         }
     }
+
+    /*Létrehoztam egy Random objektumot, ezzel generáltam egy float típusú számot, majd ezt 25-tel szorozva és 25-t hozzáadva megkaptam a 25-50 közötti random számot.
+    Az if feltételben megnéztem, hogy a szám egyezik e az int típusra kasztolt értékkel, mert ha igen, akkor egész szám, egyéb esetben nem egész szám. */
+    public void generateRandomNumber() {
+        Random random = new Random();
+        float randomNumber = random.nextFloat() * 25 + 25;
+        if (randomNumber == (int) randomNumber) {
+            System.out.print("egész szám: " + randomNumber);
+        } else {
+            System.out.print("nem egész szám: " + randomNumber);
+        }
+
+    }
+
 }
